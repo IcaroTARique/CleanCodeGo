de82415e6db1bc260ca44ff2984721c8afa864cc
Icaro Targino <icaro_rique123@hotmail.com>
2024-06-09 21:55:57 -0300

    Creating first commit with first working code (web and grpc)

---
+++ internal/usecase/create_order.go
@@ -0,0 +1,81 @@
+package usecase
+
+import (
+	"github.com/devfullcycle/20-CleanArch/internal/entity"
+	"github.com/devfullcycle/20-CleanArch/pkg/events"
+)
+
+type OrderInputDTO struct {
+	ID    string  `json:"id"`
+	Price float64 `json:"price"`
+	Tax   float64 `json:"tax"`
+}
+
+type OrderOutputDTO struct {
+	ID         string  `json:"id"`
+	Price      float64 `json:"price"`
+	Tax        float64 `json:"tax"`
+	FinalPrice float64 `json:"final_price"`
+}
+
+type CreateOrderUseCase struct {
+	OrderRepository entity.OrderRepositoryInterface
+	OrderCreated    events.EventInterface
+	EventDispatcher events.EventDispatcherInterface
+}
+
+func NewCreateOrderUseCase(
+	OrderRepository entity.OrderRepositoryInterface,
+	OrderCreated events.EventInterface,
+	EventDispatcher events.EventDispatcherInterface,
+) *CreateOrderUseCase {
+	return &CreateOrderUseCase{
+		OrderRepository: OrderRepository,
+		OrderCreated:    OrderCreated,
+		EventDispatcher: EventDispatcher,
+	}
+}
+
+func (c *CreateOrderUseCase) Execute(input OrderInputDTO) (OrderOutputDTO, error) {
+	order := entity.Order{
+		ID:    input.ID,
+		Price: input.Price,
+		Tax:   input.Tax,
+	}
+	order.CalculateFinalPrice()
+	if err := c.OrderRepository.Save(&order); err != nil {
+		return OrderOutputDTO{}, err
+	}
+
+	dto := OrderOutputDTO{
+		ID:         order.ID,
+		Price:      order.Price,
+		Tax:        order.Tax,
+		FinalPrice: order.Price + order.Tax,
+	}
+
+	c.OrderCreated.SetPayload(dto)
+	c.EventDispatcher.Dispatch(c.OrderCreated)
+
+	return dto, nil
+}
+
+func (c *CreateOrderUseCase) GetAllOrders() ([]OrderOutputDTO, error) {
+	orders, err := c.OrderRepository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var dtos []OrderOutputDTO
+	for _, order := range orders {
+		dto := OrderOutputDTO{
+			ID:         order.ID,
+			Price:      order.Price,
+			Tax:        order.Tax,
+			FinalPrice: order.FinalPrice,
+		}
+		dtos = append(dtos, dto)
+	}
+
+	return dtos, nil
+}
